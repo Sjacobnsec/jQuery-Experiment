b35dcd4d5b37540059f0d14a82fb0db08cc42241
Nsec <S.jacobnsec@gmail.com>
2015-08-13 12:30:42 -0500

    Refactor using traversing

--- application.js
+++ application.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	$("ul").text("I was replaced")
+	$('button').on('click', function() {
+		var price = $('<p>From $399.99</p>');
+		$('.vacation').append(price);
+		$('button').remove();
+	});
 });
