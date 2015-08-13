17670cc2041b42e810a28e52d72e40e4b492b4f7
Nsec <S.jacobnsec@gmail.com>
2015-08-13 12:34:59 -0500

    this to remove only clicked button

--- application.js
+++ application.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('button').on('click', function() {
 		var price = $('<p>From $399.99</p>');
 		$('.vacation').append(price);
-		$('button').remove();
+		//$('button').remove(); This removes all the buttons on the page
+		$(this).remove(); //this removes just the clicked button
 	});
 });
